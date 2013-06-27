5590a320408d3870d6bffeed26e3b7172ab83ed0
Corbin Sykes <corbin.sykes@gmail.comm>
2013-06-27 10:12:33 -0400

    sorry about the lack of commits. reused a lot of the previous movie code, just added the api. changed File.new to File.open

--- main.rb
+++ main.rb
@@ -4,5 +4,46 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'imdb'
 
+
 get '/' do
+  @movies = []
+  movies = File.new('movies.csv', 'r')
+  movies.each do |title|
+    @movies << title.split(',')
+  end
+    erb :movies
+end
+
+
+
+get '/movie/:title' do
+  @title = params[:title]
+  @single_movie = []
+  movies_file = File.new('movies.csv', 'r')
+  movies_file.each do |title|
+    title.split(',')
+    # Identifying the name of the movie
+    if title.split(',')[0] == @title
+      then @single_movie = title.split(',')
+    end
+  end
+  erb :movie
+end
+
+get '/new_movie' do
+  erb :new_movie
+end
+
+post '/new_movie' do
+  @title = params[:title]
+  @movie = Imdb::Search.new(@title).movies.first
+  movies = File.open('movies.csv', 'a+') do |title|
+   title.puts("#{@movie.title},#{@movie.year},#{@movie.director[0]},#{@movie.poster}")
+  end
+
+
+
+
+
+redirect to '/'
 end
